32339df0fc5d6905753391af45811d69efb3579e
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-11-14 16:37:47 +0530

    Added: Use lombok library for logging

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -1,13 +1,19 @@
 package com.bridgelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@Slf4j
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
-
+//Here context is used to store the configuration of SpringApplication which run with various argument
+// from that we are able to get environment and database name
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
+        log.info("Employee payroll app started in {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Employee payroll db User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
     }
 
 }
